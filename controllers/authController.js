c9733800d910b0d73795be4078bc86b5e1f0359a
Bartosz Mogilan <bmogilan@gmail.com>
2025-02-04 20:06:49 +0000

    adding the authorisation section

---
+++ controllers/authController.js
@@ -0,0 +1,40 @@
+const pool = require ("../config/db");
+const bcrypt = require ("bcrypt");
+const jwt = require("jsonwebtoken");
+
+exports.register = async (req, res) => {
+    const {username, email, password} = req.body;
+    try {
+        const userExists = await pool.query("SELECT * FROM user WHERE email = $1", [email]);
+        if (userExists.rows.length > 0) {
+            return res.status(400).json({ error: "User already exists"});
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = await pool.query ("INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING id, username, email", [username, email, hashedPassword]);
+        res.status(201).json(newUser.rows[0]);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Error registering user"});
+    }
+};
+
+exports.login = async (req, res) => {
+    const {email, password} = req.body;
+    try {
+        const userResult = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
+        if (userResult.rows.length === 0) {
+            return res.status(400).json({error: "Invalid credentials"});
+        }
+        const user = userResult.rows[0];
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({error: "Invalid credentials"});
+        }
+        const token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIN: "1h"});
+        res.json({token});
+    } catch (error) {
+        console.error(error);
+        res.status(500),json({error: "Error logging in"});
+    }
+};
+
