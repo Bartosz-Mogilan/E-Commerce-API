b045eda833f4ffdb931c9c12444cc4f20a8eac3d
Bartosz Mogilan <bmogilan@gmail.com>
2025-02-04 21:37:17 +0000

    updates

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-const PORT = process.env.PORT || 3000;
 
 dotenv.config();
 
-const exampleRoutes = require("./controllers/exampleControllers");
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,9 +14,21 @@ app.use(bodyParser.json());
 
 //Routes
 
-app.use("/exampleControllers", exampleRoutes);
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/users");
+const productRoutes = require("./routes/products");
+const cartRoutes = require("./routes/cart");
+const orderRoutes = require("./routes/orders");
 
+//Using the routes
 
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/products", productRoutes);
+app.use("/api/v1/cart", cartRoutes);
+app.use("/api/v1/orders", orderRoutes);
+
+//Default route
 
 app.get('/', (req, res) => {
     res.send("Hello, E Commerce API");
@@ -26,3 +37,4 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
